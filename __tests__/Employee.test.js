3f63500270a69301f1b0a51a46c100c6972b6a1d
Dave Sanders <davepaulsanders@gmail.com>
2022-04-20 10:39:47 -0400

    employee creation and methods working

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,27 +2,47 @@ const Employee = require("../lib/Employee");
 
 describe("Testing for Employee Parent Class", () => {
   it("creates an employee object", () => {
-    const test = new Employee("Dave", 1);
-    expect(test.name).toBe("Dave");
+    const test = new Employee("Dave", 1, "davepaulsanders@gmail.com");
+    expect(test.name).toEqual(expect.any(String));
     expect(test.id).toEqual(expect.any(Number));
-    expect(test.email).toEqual(expect.toContain(".com"));
+    expect(test.email).toEqual(expect.any(String));
   });
 
   it("gets name of employee", () => {
-    const test = new Employee("Dave", 1, "davepaulsanders@gmail.com, manager");
+    const test = new Employee(
+      "Dave",
+      1,
+      "davepaulsanders@gmail.com",
+      "manager"
+    );
     expect(test.getName()).toEqual("Dave");
   });
 
   it("gets id of employee", () => {
-    const test = new Employee("Dave", 1, "davepaulsanders@gmail.com, manager");
+    const test = new Employee(
+      "Dave",
+      1,
+      "davepaulsanders@gmail.com",
+      "manager"
+    );
     expect(test.getId()).toEqual(1);
   });
   it("gets email of employee", () => {
-    const test = new Employee("Dave", 1, "davepaulsanders@gmail.com, manager");
+    const test = new Employee(
+      "Dave",
+      1,
+      "davepaulsanders@gmail.com",
+      "manager"
+    );
     expect(test.getEmail()).toEqual("davepaulsanders@gmail.com");
   });
   it("gets tole of employee", () => {
-    const test = new Employee("Dave", 1, "davepaulsanders@gmail.com, Manager");
-    expect(test.getEmail()).toEqual("Manager");
+    const test = new Employee(
+      "Dave",
+      1,
+      "davepaulsanders@gmail.com",
+      "Employee"
+    );
+    expect(test.getRole()).toEqual("Employee");
   });
 });
